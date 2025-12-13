8bcefc5211868dc8067f0cc3c6706af6ec864863
Maksim Smal <maksimblak@mail.ru>
2025-12-13 20:15:16 +0300

    Add address zone API with caching and geocoding

--- server.mjs
+++ server.mjs
@@ -219,7 +219,7 @@ const withCors = (req, res, next) => {
   const origin = req.headers.origin || '*';
   res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Vary', 'Origin');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
